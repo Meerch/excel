9a2b2760c57ee770c9caf7f6b673c41d9210fe99
Meerch <56489774+Meerch@users.noreply.github.com>
2020-11-06 06:41:30 +0300

    Framework start (#3)
    * Finish base framework functional
    
    * comment debug
    
    * Test WebStorm UI for Git

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,23 @@
+import {DomListener} from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+    constructor($root, options = {} ) {
+        super($root, options.listeners)
+        this.name = options.name || ''
+
+    }
+
+    // Возращает шаблон компонента
+    toHTML() {
+        return ''
+    }
+
+    init() {
+        this.initDOMListeners()
+    }
+
+    destoy() {
+        this.removeDOMListeners()
+    }
+
+}
