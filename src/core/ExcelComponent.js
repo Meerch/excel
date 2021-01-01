670c440717ec6437b48509bb2016d353f9cb9927
Meerch <khukhriansk333@gmail.com>
2021-01-01 18:57:30 +0300

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,6 +4,13 @@ export class ExcelComponent extends DomListener {
     constructor($root, options = {} ) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers =[]
+
+        this.prepare()
+    }
+    // Настраиваем наш компонент до init
+    prepare() {
 
     }
 
@@ -11,13 +18,28 @@ export class ExcelComponent extends DomListener {
     toHTML() {
         return ''
     }
+    // Уведомлеяем слушателей про событие event
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+    // Подписываемся на событие event
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
+    }
 
+    // Иниализируем компонент
+    // Добавляем DOM слушателей
     init() {
         this.initDOMListeners()
     }
 
-    destoy() {
+
+    // Удаляем компонент
+    // Чистим слушателей
+    destroy() {
         this.removeDOMListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 
 }
