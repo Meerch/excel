62a489d6fbde9bd735cf0c9f8c0e66231f697baa
Meerch <khukhriansk333@gmail.com>
2021-01-10 09:10:42 +0300

    Finish redux and table logic

---
+++ src/redux/actions.js
@@ -0,0 +1,38 @@
+import {APPLY_STYLE, CHANGE_STYLE, CHANGE_TEXT, CHANGE_TITLE, TABLE_RESIZE} from './types'
+
+// Action Creator
+export function tableResize(data) {
+  return {
+    type: TABLE_RESIZE,
+    data
+  }
+}
+
+export function changeText(data) {
+  return {
+    type: CHANGE_TEXT,
+    data
+  }
+}
+
+export function changeStyles(data) {
+  return {
+    type: CHANGE_STYLE,
+    data
+  }
+}
+
+export function applyStyle(data) {
+  return {
+    type: APPLY_STYLE,
+    data
+  }
+}
+
+export function changeTitle(data) {
+  return {
+    type: CHANGE_TITLE,
+    data
+  }
+}
+
